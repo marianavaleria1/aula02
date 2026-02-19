d5011a6dfde0757abb6884281637c5cb2d0253cb
marianavaleria1 <mariana.v.sousa@aluno.senai.br>
2026-02-19 11:32:22 -0300

    entregando Exercicio 1,2 e 3

---
+++ Exercicio2.cs
@@ -0,0 +1,11 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    private static string nome = "Mariana Valéria";
+    
+    public static void Executar()
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
